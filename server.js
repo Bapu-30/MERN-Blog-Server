0fb454020b674db94cbb9bc38f95f7b09e96ff33
Bapu <1.bapu.ojha@gmail.com>
2024-05-25 11:24:37 +0530

    Notification page api fix

--- server.js
+++ server.js
@@ -773,46 +773,69 @@ server.get("/new-notification", verifyJwt, (req, res) => {
         })
 })
 
-server.post("/notifications", verifyJwt, (req, res)=>{
+server.post("/notifications", verifyJwt, (req, res) => {
     let user_id = req.user;
-    
-    let {page, filter, deletedDocCount} = req.body;
+
+    let { page, filter, deletedDocCount } = req.body;
 
     let maxLimit = 10;
 
-    let findQuery = {notification_for : user_id, user : {$ne : user_id}}
+    let findQuery = { notification_for: user_id, user: { $ne: user_id } }
 
     let skipDocs = (page - 1) * maxLimit;
 
-    if(filter != 'all'){
+    if (filter != 'all') {
         findQuery.type = filter;
     }
 
-    if(deletedDocCount){
+    if (deletedDocCount) {
         skipDocs -= deletedDocCount
     }
 
     Notification.find(findQuery)
-    .skip(skipDocs)
-    .limit(maxLimit)
-    .populate("blog", "title blog_id")
-    .populate("user", "personal_info.fullName personal_info.userName personal_info.profile_img")
-    .populate("comment", "comment")
-    .populate("replied_on_comment", "comment")
-    .populate("reply", "comment")
-    .sort({createdAt : -1})
-    .select(" createdAt type seen reply ")
-    .then(notifications =>{
-        return res.status(200).json({notifications})
-    })
-    .catch(err =>{
-        console.log(err.message);
-        return res.status(500).json({error : err.message})
+        .skip(skipDocs)
+        .limit(maxLimit)
+        .populate("blog", "title blog_id")
+        .populate("user", "personal_info.fullName personal_info.userName personal_info.profile_img")
+        .populate("comment", "comment")
+        .populate("replied_on_comment", "comment")
+        .populate("reply", "comment")
+        .sort({ createdAt: -1 })
+        .select(" createdAt type seen reply ")
+        .then(notifications => {
+            return res.status(200).json({ notifications })
+        })
+        .catch(err => {
+            console.log(err.message);
+            return res.status(500).json({ error: err.message })
 
-    })
+        })
 
 
 })
+
+server.post("/all-notifications-count", verifyJwt, (req, res) => {
+    let user_id = req.user;
+    let { filter } = req.body;
+
+    let findQuery = { notification_for: user_id, user: { $ne: user_id } }
+
+    if (filter != 'all') {
+        findQuery.type = filter;
+    }
+
+    Notification.countDocuments(findQuery)
+        .then(count => {
+            return res.status(200).json({ totalDocs: count })
+        })
+        .catch(err => {
+            return res.status(500).json({ error: err.message })
+        })
+
+
+})
+
+
 
 
 
